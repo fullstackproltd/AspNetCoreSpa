e4fb84dbb0ff77b330de1c8b1ff3ae22ee6b9961
crh225 <crh225@users.noreply.github.com>
2016-08-16 14:03:43 -0500

    fix: lint issue cleansing; chore: updated es6-promise; tst: added two specs for models (#7)
    * fix: lint issue cleansing
    * test: added two test files for login.model and register.model
    * chore: updating es6-promise
    * test: fixing typos
    * fix: removing comment from tsconfig file. it is causing typedoc to fail.
    * test: adding spec for user.model

---
+++ Client/modules/+login/login.model.spec.ts
@@ -0,0 +1,16 @@
+import { LoginModel } from './login.model';
+
+describe('Login Model', () => {
+  it('has email', () => {
+    let loginModel: LoginModel = {email: 'test@test.test', password: 'Super Cat', rememberMe: true};
+    expect(loginModel.email).toEqual('test@test.test');
+  });
+  it('has password', () => {
+    let loginModel: LoginModel = {email: 'test@test.test', password: 'Super Cat', rememberMe: true};
+    expect(loginModel.password).toEqual('Super Cat');
+  });
+  it('has rememberMe', () => {
+    let loginModel: LoginModel = {email: 'test@test.test', password: 'Super Cat', rememberMe: true};
+    expect(loginModel.rememberMe).toEqual(true);
+  });
+});
