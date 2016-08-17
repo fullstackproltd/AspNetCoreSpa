ca52b29e4ffb7f1364fefb67785ed76ca637e6b1
Asad Sahi <asadazmat@gmail.com>
2016-08-17 14:32:51 +0100

    removed console

--- Client/modules/home/home.component.ts
+++ Client/modules/home/home.component.ts
@@ -8,8 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   constructor() { }
 
-  ngOnInit() {
-    console.log('asdf')
-    }
+  ngOnInit() {  }
 
 }
