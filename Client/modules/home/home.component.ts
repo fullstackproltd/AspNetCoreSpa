5daee394531bf9f6f7e86942c59716d9744aa607
Asad Sahi <asadazmat@gmail.com>
2016-08-20 14:56:45 +0100

    ci changes

--- Client/modules/home/home.component.ts
+++ Client/modules/home/home.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   constructor() { }
 
-  ngOnInit() {  }
+  ngOnInit() {
+    console.log('home component loaded');
+  }
 
 }
