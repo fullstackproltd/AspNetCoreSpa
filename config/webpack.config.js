50628bcf64ced00d7088aa8140ce8df33d577442
Asad Sahi <asadazmat@gmail.com>
2016-11-14 12:53:54 +0000

    simplified fonts loader

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var helpers = require('./helpers');
 var webpack = require('webpack');
 var merge = require('extendify')({ isDeep: true, arrays: 'concat' });
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -8,11 +9,19 @@ var devConfig = require('./webpack.config.dev');
 var prodConfig = require('./webpack.config.prod');
 var isDevelopment = process.env.ASPNETCORE_ENVIRONMENT === 'Development';
 
-console.log("==========Dev Mode = " + isDevelopment + " ============" )
+console.log("==========Dev Mode = " + isDevelopment + " ============")
 
 module.exports = merge({
     resolve: {
-        extensions: ['.js', '.ts']
+        /*
+       * An array of extensions that should be used to resolve modules.
+       *
+       * See: http://webpack.github.io/docs/configuration.html#resolve-extensions
+       */
+        extensions: ['.ts', '.js', '.json', '.scss', '.html'],
+
+        // An array of directory names to be resolved to the current directory
+        modules: [helpers.root('Client'), 'node_modules'],
     },
     module: {
         rules: [
@@ -21,22 +30,9 @@ module.exports = merge({
             { test: /\.css/, loader: extractCSS.extract(['css']) },
             { test: /\.scss$/, loaders: ['raw-loader', 'sass-loader?sourceMap'] },
             { test: /\.json$/, loader: 'json-loader' },
-            {
-                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url-loader?limit=10000&mimetype=application/font-woff"
-            }, {
-                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url-loader?limit=10000&mimetype=application/font-woff"
-            }, {
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url-loader?limit=10000&mimetype=application/octet-stream"
-            }, {
-                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "file-loader"
-            }, {
-                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url-loader?limit=10000&mimetype=image/svg+xml"
-            }]
+            { test: /\.(woff2?|ttf|eot|svg)$/, loader: 'url-loader?limit=10000' },
+            { test: /\.(jpg|png|gif)$/, loader: 'file-loader' }
+        ]
     },
     entry: {
         'main': './Client/main.ts'
