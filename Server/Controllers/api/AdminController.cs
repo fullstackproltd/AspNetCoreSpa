a55c5d97e8c0386b18e642a65501ad52e9941c9f
Asad <asadazmat@gmail.com>
2016-12-17 14:07:46 +0000

    correct namespace

--- Server/Controllers/api/AdminController.cs
+++ Server/Controllers/api/AdminController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace AspNetCoreSPA.Server.Controllers.api
+namespace AspNetCoreSpa.Server.Controllers.api
 {
     [Authorize(Roles = "Admin")]
     [Route("api/[controller]")]
