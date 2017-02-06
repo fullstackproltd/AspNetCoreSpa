2167952315caab1d8dd3bd256b28e20bb2ab68ba
Asad Sahi <asadazmat@gmail.com>
2017-02-06 16:48:36 +0000

    appveyor build

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -25,7 +25,31 @@ module.exports = webpackMerge(commonConfig, {
         new OptimizeJsPlugin({
             sourceMap: false
         }),
-        new BundleAnalyzerPlugin(),
+        new BundleAnalyzerPlugin({
+            // Can be `server`, `static` or `disabled`. 
+            // In `server` mode analyzer will start HTTP server to show bundle report. 
+            // In `static` mode single HTML file with bundle report will be generated. 
+            // In `disabled` mode you can use this plugin to just generate Webpack Stats JSON file by setting `generateStatsFile` to `true`. 
+            analyzerMode: 'static',
+            // Port that will be used in `server` mode to start HTTP server. 
+            // analyzerPort: 8888,
+            // Path to bundle report file that will be generated in `static` mode. 
+            // Relative to bundles output directory. 
+            reportFilename: 'report.html',
+            // Automatically open report in default browser 
+            openAnalyzer: false,
+            // If `true`, Webpack Stats JSON file will be generated in bundles output directory 
+            generateStatsFile: false,
+            // Name of Webpack Stats JSON file that will be generated if `generateStatsFile` is `true`. 
+            // Relative to bundles output directory. 
+            statsFilename: 'stats.json',
+            // Options for `stats.toJson()` method. 
+            // For example you can exclude sources of your modules from stats file with `source: false` option. 
+            // See more options here: https://github.com/webpack/webpack/blob/webpack-1/lib/Stats.js#L21 
+            statsOptions: null,
+            // Log level. Can be 'info', 'warn', 'error' or 'silent'. 
+            logLevel: 'info'
+        }),
         new webpack.optimize.UglifyJsPlugin({
             beautify: false,
             comments: false,
