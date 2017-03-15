e89e01008d9293d718422818d0b6b3c7715fca4e
Asad Sahi <asadazmat@gmail.com>
2017-03-15 11:42:09 +0000

    ng-bootstrap upgraded & animations examples refactoring

--- Client/app/+examples/animations/animations.routes.ts
+++ Client/app/+examples/animations/animations.routes.ts
@@ -1,9 +1,16 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { AnimationsComponent } from './animations.component';
+import { AnimationsBasicExamplesComponent } from './basic-examples/basic-examples.component';
+import { AnimationsRepeatingItemsComponent } from './repeating-items/repeating-items.component';
 
 const routes: Routes = [
-  { path: '', component: AnimationsComponent }
+  {
+    path: '', component: AnimationsComponent, children: [
+      { path: 'basicanimation', component: AnimationsBasicExamplesComponent },
+      { path: 'repeatinganimations', component: AnimationsRepeatingItemsComponent }
+    ]
+  },
 ];
 
 export const routing = RouterModule.forChild(routes);
