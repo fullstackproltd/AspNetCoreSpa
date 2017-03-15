929b36b235082a05b54e5d282a37e1442bfdffff
Asad Sahi <asadazmat@gmail.com>
2017-03-15 09:52:04 +0000

    added missing rxjs operators

--- Client/main.ts
+++ Client/main.ts
@@ -3,10 +3,15 @@ import './styles/vendor.css';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/first';
+import 'rxjs/add/operator/mapTo';
+import 'rxjs/add/operator/scan';
+import 'rxjs/add/operator/bufferCount';
+import 'rxjs/add/operator/startWith';
 import 'rxjs/add/operator/finally';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/interval';
 import 'rxjs/add/observable/throw';
+import 'rxjs/add/observable/merge';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
