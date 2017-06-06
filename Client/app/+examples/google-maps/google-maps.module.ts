f4d92ff23bfd06c81c2925322e18e515de5d4847
Asad Sahi <asadazmat@gmail.com>
2017-06-06 11:31:02 +0100

    google maps package upgrade

--- Client/app/+examples/google-maps/google-maps.module.ts
+++ Client/app/+examples/google-maps/google-maps.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { AgmCoreModule } from 'angular2-google-maps/core';
+import { AgmCoreModule } from '@agm/core';
 
 import { SharedModule } from '../../shared/shared.module';
 import { routing } from './google-maps.routes';
