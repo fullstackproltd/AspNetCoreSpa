bcc1fa4d9795c47b54e191c533f97a98d3c50e53
Asad Sahi <asadazmat@gmail.com>
2017-11-21 18:44:33 +0000

    ignored

--- obj/Debug/netcoreapp2.0/fr/AspNetCoreSpa.resources.cs
+++ obj/Debug/netcoreapp2.0/fr/AspNetCoreSpa.resources.cs
@@ -9,7 +9,6 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspNetCoreSpa")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute(" ")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("Package Description")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspNetCoreSpa")]
