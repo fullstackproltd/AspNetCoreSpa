4a190e423354550b7f29f535dba4b411900c0baf
Asad Sahi <asadazmat@gmail.com>
2017-11-26 23:41:13 +0000

    fix for image url

--- ClientApp/src/app/app.service.ts
+++ ClientApp/src/app/app.service.ts
@@ -1,12 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class AppService {
-    constructor(private httpClient: HttpClient) { }
+    constructor(private httpClient: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
     getData(): Promise<any> {
         return new Promise((resolve) => {
-            this.httpClient.get<any>('http://localhost:5000/api/applicationdata')
+            this.httpClient.get<any>(`${this.baseUrl}api/applicationdata`)
                 .subscribe(res => {
                     // console.log(res);
                     (<any>window).appData = {
