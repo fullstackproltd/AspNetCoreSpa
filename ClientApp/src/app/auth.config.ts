9d412d0965a81ca98683fdf371eade15ff1d0239
Asad <asadazmat@gmail.com>
2017-11-27 15:28:16 +0000

    fix for oidc ssl

--- ClientApp/src/app/auth.config.ts
+++ ClientApp/src/app/auth.config.ts
@@ -1,4 +1,5 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
+import { environment } from '../environments/environment';
 
 export function authConfig(url: string): AuthConfig {
 
@@ -10,7 +11,7 @@ export function authConfig(url: string): AuthConfig {
         // The SPA's id. The SPA is registered with this id at the auth-server
         clientId: 'aspnetcorespa',
         tokenEndpoint: url + '/connect/token',
-        requireHttps: false,
+        requireHttps: environment.production,
         // set the scope for the permissions the client should request
         // The first three are defined by OIDC.
         scope: 'openid profile email offline_access client_id roles'
