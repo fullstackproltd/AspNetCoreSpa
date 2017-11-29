0c5a2afcffffab0581d1a825850307cccd42b3ef
Asad <asadazmat@gmail.com>
2017-11-29 12:03:08 +0000

    removed comments

--- Startup.cs
+++ Startup.cs
@@ -29,18 +29,6 @@ namespace AspNetCoreSpa
 
             Helpers.SetupSerilog();
 
-            // var builder = new ConfigurationBuilder()
-            //                .SetBasePath(env.ContentRootPath)
-            //                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-            //                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-            //                .AddEnvironmentVariables();
-            // if (env.IsDevelopment())
-            // {
-            //     // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
-            //     builder.AddUserSecrets<Startup>();
-            // }
-
-            // Configuration = builder.Build();
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
