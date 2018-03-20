3c645c08933f3f6c36c3b80eb467944fa73f8e2a
Asad <asadazmat@gmail.com>
2018-03-20 14:46:19 +0000

    fixed 210

--- ClientApp/src/app/+chat/chat.component.ts
+++ ClientApp/src/app/+chat/chat.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { HubConnection } from '@aspnet/signalr-client';
+import { HubConnection } from '@aspnet/signalr';
 
 @Component({
   selector: 'appc-chat',
