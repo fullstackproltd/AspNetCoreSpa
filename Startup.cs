fdba96ba3dc204b4a2db8ee0fd5dbb604e4b2c42
Asad <asadazmat@gmail.com>
2018-04-11 15:36:39 +0100

    new cli changes

--- Startup.cs
+++ Startup.cs
@@ -145,7 +145,7 @@ namespace AspNetCoreSpa
 
                           if (env.IsDevelopment())
                           {
-                              //   spa.UseAngularCliServer(;npmScript: "start");
+                              // spa.UseAngularCliServer(npmScript: "start");
                               //   OR
                               spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
                           }
