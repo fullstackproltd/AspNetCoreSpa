a720f84613a2d2bd7dc9b3fd8e2cf3d0ab91ce5e
unknown <asadazmat@gmail.com>
2018-04-12 21:58:56 +0100

    removed tools

--- Program.cs
+++ Program.cs
@@ -14,11 +14,11 @@ namespace AspNetCoreSpa
         {
             var host = CreateWebHostBuilder(args).Build();
             // http://odetocode.com/blogs/scott/archive/2016/09/20/database-migrations-and-seeding-in-asp-net-core.aspx
-            ProcessDbCommands.Process(args, host);
+            // ProcessDbCommands.Process(args, host);
             host.Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>    
                    WebHost.CreateDefaultBuilder(args)
                        .UseStartup<Startup>();
     }
