a1a2a1c6d5bed413dde03f6fd66818c61af9cca0
Asad Sahi <asadazmat@gmail.com>
2018-08-23 21:27:53 +0100

    unit test project added

---
+++ tests/AspNetCoreSpa.Web.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AspNetCoreSpa.Web.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void DummyTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
