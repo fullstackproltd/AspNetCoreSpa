83e2c4b5563be93526ec4eea4fd390955f604f2a
Viktar Hushchynski <viktar.hushchynski@gmail.com>
2020-01-03 19:32:30 -0500

    Fix Too Many Redirects after deploying solution to Azure

--- src/AspNetCoreSpa.Web/Startup.cs
+++ src/AspNetCoreSpa.Web/Startup.cs
@@ -17,182 +17,183 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 
 namespace AspNetCoreSpa.Web
 {
-    public class Startup
-    {
-        // Order or run
-        //1) Constructor
-        //2) Configure services
-        //3) Configure
-        private IWebHostEnvironment HostingEnvironment { get; }
-        public static IConfiguration Configuration { get; set; }
+	public class Startup
+	{
+		// Order or run
+		//1) Constructor
+		//2) Configure services
+		//3) Configure
+		private IWebHostEnvironment HostingEnvironment { get; }
+		public static IConfiguration Configuration { get; set; }
 
-        public Startup(IConfiguration configuration, IWebHostEnvironment env)
-        {
-            HostingEnvironment = env;
-            Configuration = configuration;
-        }
+		public Startup(IConfiguration configuration, IWebHostEnvironment env)
+		{
+			HostingEnvironment = env;
+			Configuration = configuration;
+		}
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.Configure<CookiePolicyOptions>(options =>
+			{
+				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+				options.CheckConsentNeeded = context => true;
+				options.MinimumSameSitePolicy = SameSiteMode.None;
+			});
 
-            services.AddOptions();
+			services.AddOptions();
 
-            services.AddResponseCompression();
+			services.AddResponseCompression();
 
-            services.AddCustomDbContext();
+			services.AddCustomDbContext();
 
-            services.AddMemoryCache();
+			services.AddMemoryCache();
 
-            services.AddHealthChecks();
+			services.AddHealthChecks();
 
-            services.RegisterCustomServices();
+			services.RegisterCustomServices();
 
-            services.AddSignalR()
-                .AddMessagePackProtocol();
+			services.AddSignalR()
+				.AddMessagePackProtocol();
 
-            services.AddCustomLocalization(HostingEnvironment);
+			services.AddCustomLocalization(HostingEnvironment);
 
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                            .AddJwtBearer(options =>
-                            {
-                                // base-address of your identity server
-                                options.Authority = Configuration["StsAuthority"];
-                                // name of the API resource
-                                options.Audience = "spa-api";
-                            });
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+							.AddJwtBearer(options =>
+							{
+								// base-address of your identity server
+								options.Authority = Configuration["StsAuthority"];
+								// name of the API resource
+								options.Audience = "spa-api";
+							});
 
-            services.AddCustomizedMvc();
+			services.AddCustomizedMvc();
 
-            // In production, the Angular files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp/dist/aspnetcorespa";
-            });
+			// In production, the Angular files will be served from this directory
+			services.AddSpaStaticFiles(configuration =>
+			{
+				configuration.RootPath = "ClientApp/dist/aspnetcorespa";
+			});
 
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AspNetCoreSpa", Version = "v1" });
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "AspNetCoreSpa", Version = "v1" });
 
-                var security = new Dictionary<string, IEnumerable<string>>
-                {
-                    {"Bearer", new string[] { }},
-                };
+				var security = new Dictionary<string, IEnumerable<string>>
+				{
+					{"Bearer", new string[] { }},
+				};
 
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey
-                });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
-                        },
-                        new[] { "readAccess", "writeAccess" }
-                    }
-                });
+				c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+				{
+					Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+					Name = "Authorization",
+					In = ParameterLocation.Header,
+					Type = SecuritySchemeType.ApiKey
+				});
+				c.AddSecurityRequirement(new OpenApiSecurityRequirement
+				{
+					{
+						new OpenApiSecurityScheme
+						{
+							Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
+						},
+						new[] { "readAccess", "writeAccess" }
+					}
+				});
 
-            });
+			});
 
-            services.AddAutoMapper(typeof(AutoMapperProfile));
-        }
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseHealthChecks("/health");
+			services.AddAutoMapper(typeof(AutoMapperProfile));
+		}
+		public void Configure(IApplicationBuilder app)
+		{
+			app.UseHealthChecks("/health");
 
-            // app.AddCustomSecurityHeaders();
+			// app.AddCustomSecurityHeaders();
 
-            if (HostingEnvironment.IsDevelopment())
-            {
-                app.AddDevMiddlewares();
-            }
-            else
-            {
-                app.UseHsts();
-                app.UseResponseCompression();
-            }
+			if (HostingEnvironment.IsDevelopment())
+			{
+				app.AddDevMiddlewares();
+			}
+			else
+			{
+				app.UseHsts();
+				app.UseResponseCompression();
+			}
 
-            app.AddCustomLocalization();
+			app.AddCustomLocalization();
 
-            app.UseHttpsRedirection();
 
-            // https://github.com/openiddict/openiddict-core/issues/518
-            // And
-            // https://github.com/aspnet/Docs/issues/2384#issuecomment-297980490
-            var forwarOptions = new ForwardedHeadersOptions
-            {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-            };
-            forwarOptions.KnownNetworks.Clear();
-            forwarOptions.KnownProxies.Clear();
+			// https://github.com/openiddict/openiddict-core/issues/518
+			// And
+			// https://github.com/aspnet/Docs/issues/2384#issuecomment-297980490
+			var forwarOptions = new ForwardedHeadersOptions
+			{
+				ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+			};
+			forwarOptions.KnownNetworks.Clear();
+			forwarOptions.KnownProxies.Clear();
 
-            app.UseForwardedHeaders(forwarOptions);
+			app.UseForwardedHeaders(forwarOptions);
 
-            app.UseAuthentication();
+			app.UseHttpsRedirection();
 
-            app.UseStaticFiles();
+			app.UseAuthentication();
 
-            app.UseSpaStaticFiles();
+			app.UseStaticFiles();
 
-            app.UseRouting();
+			app.UseSpaStaticFiles();
 
-            app.UseCookiePolicy();
+			app.UseRouting();
 
-            app.UseAuthorization();
+			app.UseCookiePolicy();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                                   name: "default",
-                                   pattern: "{controller}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
+			app.UseAuthorization();
 
-                endpoints.MapHub<Chat>("/chathub");
-                endpoints.MapHub<ShapeHub>("/shapeHub");
-            });
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllerRoute(
+								   name: "default",
+								   pattern: "{controller}/{action=Index}/{id?}");
+				endpoints.MapRazorPages();
 
-            app.UseSpa(spa =>
-                      {
-                          spa.Options.SourcePath = "ClientApp";
+				endpoints.MapHub<Chat>("/chathub");
+				endpoints.MapHub<ShapeHub>("/shapeHub");
+			});
 
-                          /*
+			app.UseSpa(spa =>
+					  {
+						  spa.Options.SourcePath = "ClientApp";
+
+						  /*
                           // If you want to enable server-side rendering (SSR),
                           // [1] In AspNetCoreSpa.csproj, change the <BuildServerSideRenderer> property
                           //     value to 'true', so that the SSR bundle is built during publish
                           // [2] Uncomment this code block
                           */
 
-                          //   spa.UseSpaPrerendering(options =>
-                          //    {
-                          //        options.BootModulePath = $"{spa.Options.SourcePath}/dist-server/main.bundle.js";
-                          //        options.BootModuleBuilder = env.IsDevelopment() ? new AngularCliBuilder(npmScript: "build:ssr") : null;
-                          //        options.ExcludeUrls = new[] { "/sockjs-node" };
-                          //        options.SupplyData = (requestContext, obj) =>
-                          //        {
-                          //          //  var result = appService.GetApplicationData(requestContext).GetAwaiter().GetResult();
-                          //          obj.Add("Cookies", requestContext.Request.Cookies);
-                          //        };
-                          //    });
+						  //   spa.UseSpaPrerendering(options =>
+						  //    {
+						  //        options.BootModulePath = $"{spa.Options.SourcePath}/dist-server/main.bundle.js";
+						  //        options.BootModuleBuilder = env.IsDevelopment() ? new AngularCliBuilder(npmScript: "build:ssr") : null;
+						  //        options.ExcludeUrls = new[] { "/sockjs-node" };
+						  //        options.SupplyData = (requestContext, obj) =>
+						  //        {
+						  //          //  var result = appService.GetApplicationData(requestContext).GetAwaiter().GetResult();
+						  //          obj.Add("Cookies", requestContext.Request.Cookies);
+						  //        };
+						  //    });
 
-                          if (HostingEnvironment.IsDevelopment())
-                          {
-                              spa.UseAngularCliServer(npmScript: "start");
-                              //   OR
-                              //spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
-                          }
-                      });
-        }
-    }
+						  if (HostingEnvironment.IsDevelopment())
+						  {
+							  spa.UseAngularCliServer(npmScript: "start");
+							  //   OR
+							  //spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+						  }
+					  });
+		}
+	}
 }
